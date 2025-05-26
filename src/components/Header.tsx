29faf04c7ba6e0f86401117dd63a9e138e0f4f8e
fuzail <mfuzail7820@gmail.com>
2025-05-26 20:31:05 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header layout="grid">
    
    with the text content:
    
        My NotesSwitch to dark mode
    
    Relevant files:
    
        - https://6000-firebase-studio-1748282040086.cluster-ancjwrkgr5dvux4qug5rbzyc2y.cloudworkstations.dev/_next/static/chunks/node_modules_ef03982d._.js
        - https://6000-firebase-studio-1748282040086.cluster-ancjwrkgr5dvux4qug5rbzyc2y.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748282040086.cluster-ancjwrkgr5dvux4qug5rbzyc2y.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    make the header text color more darker

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export function Header({ layout }: HeaderProps) {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-background border-b">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
-        <h1 className="text-3xl md:text-4xl font-pixel text-primary">{title}</h1>
+        <h1 className="text-3xl md:text-4xl font-pixel text-foreground">{title}</h1>
         <ThemeToggle />
       </div>
     </header>
