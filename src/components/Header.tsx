6471613150f23012050dd069a1fe56e6fec4a5c0
fuzail <mfuzail7820@gmail.com>
2025-05-26 18:16:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the text content:
    
        My NotesSwitch to light mode
    
    Relevant files:
    
        - https://6000-firebase-studio-1748282040086.cluster-ancjwrkgr5dvux4qug5rbzyc2y.cloudworkstations.dev/_next/static/chunks/node_modules_ef03982d._.js
        - https://6000-firebase-studio-1748282040086.cluster-ancjwrkgr5dvux4qug5rbzyc2y.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748282040086.cluster-ancjwrkgr5dvux4qug5rbzyc2y.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    Change from My Notes to Pixel Notes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-background border-b">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
-        <h1 className="text-3xl md:text-4xl font-pixel text-primary">My Notes</h1>
+        <h1 className="text-3xl md:text-4xl font-pixel text-primary">Pixel Notes</h1>
         <ThemeToggle />
       </div>
     </header>
